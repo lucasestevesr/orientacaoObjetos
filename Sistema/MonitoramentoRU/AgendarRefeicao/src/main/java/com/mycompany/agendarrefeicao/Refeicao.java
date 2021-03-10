d0452cd77a3a900298183fe0a68a65adb15f5dd4
twn-vctr <tawan.victor@engenharia.ufjf.br>
2021-03-10 01:26:42 -0300

    Agendar Refeição

---
+++ Sistema/MonitoramentoRU/AgendarRefeicao/src/main/java/com/mycompany/agendarrefeicao/Refeicao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.agendarrefeicao;
+
+/**
+ *
+ * @author tawan
+ */
+public class Refeicao {
+    private String diaSemana;
+    private String turnoRefeicao;
+    private String horario;
+    
+    public Refeicao(String tipoRefeicao, String turnoRefeicao, String horario){
+        this.diaSemana = diaSemana;
+        this.turnoRefeicao = turnoRefeicao;
+        this.horario = horario;
+    }
+}
